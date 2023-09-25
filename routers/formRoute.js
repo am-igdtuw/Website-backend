be778fc49fdd5fbba6850e256bd04a65ea6f6169
Harshita2025 <98738210+acecoder7@users.noreply.github.com>
2023-09-25 20:14:00 +0530

    backend deployed-- added final endpoint link

--- routers/formRoute.js
+++ routers/formRoute.js
@@ -5,11 +5,11 @@ import express from "express";
 const router = express.Router();
 
 
-router.post("/collaborate", createCollaborateForm);
+router.post("/collaborate", createCollaborateForm);    // https://am-website-w70g.onrender.com/api/collaborate
 
-router.post("/sponsor", createSponsorForm);
+router.post("/sponsor", createSponsorForm);    // https://am-website-w70g.onrender.com/api/sponsor
 
-router.post("/faq", createFaqQuery);
+router.post("/faq", createFaqQuery);   //https://am-website-w70g.onrender.com/api/faq
 
 router.get("/collaborate", getallCForms);
 
